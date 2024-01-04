7be2b031081934e3b11e1a0037ce576e36302ed5
Dipti2u4u <diptiprasaddas50@gmail.com>
2024-01-04 15:23:25 +0530

    just for practice

---
+++ practice/src/Components/Navbar.jsx
@@ -0,0 +1,125 @@
+/*import React from "react";
+import { Menu, Dropdown } from "antd";
+import {
+  HomeOutlined,
+  SettingOutlined,
+  EnvironmentOutlined,
+  AimOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+const Navbar = () => {
+  const profileMenu = (
+    <Menu style={{ background: "#f7f7f7", border: "1px solid #ddd" }}>
+      <Menu.Item key="profile">
+        <AimOutlined style={{ marginRight: "5px", color: "#FF6C22" }} />
+        Use My Current Location
+      </Menu.Item>
+      <Menu.Item key="logout">Saved Address</Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Menu
+      mode="horizontal"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px 50px",
+      }}
+    >
+      <Menu.Item
+        key="home"
+        icon={<HomeOutlined />}
+        style={{ marginRight: "auto" }}
+      >
+        Home
+      </Menu.Item>
+      <Dropdown overlay={profileMenu} placement="bottomCenter">
+        <span style={{ cursor: "pointer", color: "#FF6C22" }}>
+          <EnvironmentOutlined
+            style={{ marginRight: "5px", color: "#FF6C22" }}
+          />
+          Location
+        </span>
+      </Dropdown>
+      <SubMenu
+        key="settings"
+        icon={<SettingOutlined />}
+        title="Settings"
+        style={{ marginLeft: "auto" }}
+      >
+        <Menu.Item key="account">Account Settings</Menu.Item>
+        <Menu.Item key="preferences">Preferences</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Navbar;*/
+
+import React from "react";
+import { Menu, Dropdown } from "antd";
+import {
+  HomeOutlined,
+  SettingOutlined,
+  EnvironmentOutlined,
+  AimOutlined,
+} from "@ant-design/icons";
+import { FaLocationArrow } from "react-icons/fa";
+
+const { SubMenu } = Menu;
+
+const Navbar = () => {
+  const profileMenu = (
+    <Menu style={{ background: "#f7f7f7", border: "1px solid #ddd" }}>
+      <Menu.Item key="profile">
+        <AimOutlined style={{ marginRight: "5px", color: "#FF6C22" }} />
+        Use My Current Location
+      </Menu.Item>
+      <Menu.Item key="logout">
+        <FaLocationArrow style={{ marginRight: "5px", color: "#ff6C22" }} />{" "}
+        Saved Address
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Menu
+      mode="horizontal"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px 50px",
+      }}
+    >
+      <Menu.Item
+        key="home"
+        icon={<HomeOutlined />}
+        style={{ marginRight: "auto" }}
+      >
+        Home
+      </Menu.Item>
+      <Dropdown overlay={profileMenu} placement="bottomCenter">
+        <span style={{ cursor: "pointer", color: "#FF6C22" }}>
+          <EnvironmentOutlined
+            style={{ marginRight: "5px", color: "#FF6C22" }}
+          />
+          Location
+        </span>
+      </Dropdown>
+      <SubMenu
+        key="settings"
+        icon={<SettingOutlined />}
+        title="Settings"
+        style={{ marginLeft: "auto" }}
+      >
+        <Menu.Item key="account">Account Settings</Menu.Item>
+        <Menu.Item key="preferences">Preferences</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Navbar;
